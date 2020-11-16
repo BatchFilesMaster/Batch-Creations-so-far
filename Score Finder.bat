63f0451dedb8e45469d746a8170db53455ace57c
BatchFilesMaster <73325895+BatchFilesMaster@users.noreply.github.com>
2020-11-16 13:25:59 -0800

    Add files via upload

---
+++ Score Finder.bat
@@ -0,0 +1,528 @@
+@echo off
+title yiudhdsuSWDEWEJIFJJ
+
+:starting
+echo Type first score
+SET /p searchString=
+set test=%searchString%
+
+CALL SET keyRemoved=%%searchString:a=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:-=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:b=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:c=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:d=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:e=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:f=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:g=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:h=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:i=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:j=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:k=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:l=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:m=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:n=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:o=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:p=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:q=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:r=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:s=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:t=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:u=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:v=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:w=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:x=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:y=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchString:z=%%
+
+IF NOT "x%keyRemoved%"=="x%searchString%" (
+goto nope
+)
+CLS
+echo Type next score
+SET /p searchSting=
+
+CALL SET keyRemoved=%%searchSting:a=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:-=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+if %searchSting%==%searchString% goto nope
+CALL SET keyRemoved=%%searchSting:b=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:c=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:d=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:e=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:f=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:g=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:h=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:i=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:j=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:k=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:l=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:m=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:n=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:o=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:p=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:q=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:r=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:s=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:t=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:u=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:v=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:w=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:x=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:y=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSting:z=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSting%" (
+goto nope
+)
+CLS
+echo Type final score
+SET /p searchSing=
+
+
+CALL SET keyRemoved=%%searchSing:a=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:-=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:%searchString%=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:%searchSting%=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:b=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:c=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:d=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:e=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:f=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:g=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:h=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:i=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:j=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:k=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:l=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:m=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:n=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:o=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:p=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:q=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:r=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:s=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:t=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:u=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:v=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:w=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:x=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:y=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+CALL SET keyRemoved=%%searchSing:z=%%
+
+IF NOT "x%keyRemoved%"=="x%searchSing%" (
+goto nope
+)
+:Calculating
+CLS
+echo We are calculating...
+set /a one=%searchstring%
+set /a two=%searchsting%
+set /a three=%searchsing%
+set /a onedone=0
+set /a twodone=0
+set /a threedone=0
+set /a Chooser1=0
+set /a Chooser2=0
+set /a Chooser3=0
+:repeatuntildone
+if %one% GTR %two% goto onethree
+if %one% GTR %three% goto onesecond
+goto onelast
+:onethree
+if %one% GTR %three% goto onefirst
+goto onesecond
+:onefirst
+set /a Chooser1=%Chooser1% +1
+goto next
+:onesecond
+set /a Chooser2=%Chooser2% +1
+set /a onedone=%onedone% +1
+goto next
+:onethree
+set %numthree%=One
+set /a Chooser3=%Chooser3% +1
+goto next
+:onelast
+set /a Chooser3=1
+:next
+if %two% GTR %one% goto twothree
+if %two% GTR %three% goto twosecond
+goto twolast
+:twothree
+if %two% GTR %three% goto twofirst
+goto twosecond
+:twosecond
+set /a Chooser2=%Chooser2% +2
+set /a twodone=%twodone% +1
+goto nextxt
+:twofirst
+set /a Chooser1=%Chooser1% +2
+set /a twodone=%twodone% +1
+goto nextxt
+:twolast
+set /a Chooser3=%Chooser3% +2
+set /a twodone=%twodone% +1
+goto nextxt
+:nextxt
+if %threedone%==1 goto scores
+if %three% GTR %one% goto threetwo
+if %three% GTR %two% goto threesecond
+goto threelast
+:threetwo
+if %three% GTR %two% goto threefirst
+goto threesecond
+:threesecond
+set /a Chooser2=%Chooser2% +3
+set /a threedone=%threedone% +1
+goto scores
+:threelast
+set /a Chooser3=%Chooser3% +3
+set /a threedone=%threedone% +1
+goto scores
+:threefirst
+set /a Chooser1=%Chooser1% +3
+set /a threedone=%threedone% +1
+goto scores
+
+:scores
+if %Chooser1%==1 set Chooser1score=%searchString%
+if %Chooser1%==2 set Chooser1score=%searchSting%
+if %Chooser1%==3 set Chooser1score=%searchSing%
+if %Chooser2%==1 set Chooser2score=%searchString%
+if %Chooser2%==2 set Chooser2score=%searchSting%
+if %Chooser2%==3 set Chooser2score=%searchSing%
+if %Chooser3%==1 set Chooser3score=%searchString%
+if %Chooser3%==2 set Chooser3score=%searchSting%
+if %Chooser3%==3 set Chooser3score=%searchSing%
+goto score
+:score
+echo And done! It's amazing how fast batch code works!
+timeout 4 >NUL
+CLS
+echo #1: Score %Chooser1% with %Chooser1score%
+echo #2: Score %Chooser2% with %Chooser2score%
+echo #3: Score %Chooser3% with %Chooser3score%
+pause
+goto uyiwjudeijuds >NUL
+:nope
+echo nope
+pause
